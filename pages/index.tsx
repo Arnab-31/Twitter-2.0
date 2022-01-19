3eaf55de69b9cd8db97ef6df7b9928ecc7fba8bf
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2022-01-19 22:53:21 +0530

    feat: Google Authentication added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed'
+import { getProviders, getSession, useSession } from "next-auth/react";
+import { SessionProvider } from "next-auth/react"
+import { FC } from 'react';
+import Login from "../components/Login";
 
-export default function Home() {
+interface HomeProps {
+  trendingResults: any,
+  followResults: any,
+  providers: any
+}
+
+const Home:FC<HomeProps> = ({ trendingResults, followResults, providers })  => {
+  // <SessionProvider>
+  //   {useSession() ? <Login />: null}
+  // </SessionProvider>
+  const { data: session } = useSession();
+
+  if (!session) return <Login  providers={providers}/>;
+
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
@@ -12,8 +30,31 @@ export default function Home() {
       
       <main className="bg-black min-h-screen flex min-w-full max-w-[1500px]">
           <Sidebar/>
-          <Feed />
+          <Feed /> 
+          {session.user.name}
       </main> 
     </div>
   )
 }
+
+export async function getServerSideProps(context: any) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+ 
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
+}
+
+export default Home;

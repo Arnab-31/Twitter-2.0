f9b1d65c1a6f7ff415b779bf532af89f2b23d5b3
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2022-01-22 01:20:52 +0530

    feat: Post UI in feed added

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,43 @@
 
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
 const Feed = () => {
+
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  // MESSY
+  // useEffect(() => {
+  //   const unsubscribe = onSnapshot(
+  //     query(collection(db, "posts"), orderBy("timestamp", "desc")),
+  //     (snapshot) => {
+  //       setPosts(snapshot.docs);
+  //     }
+  //   );
+
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, [db]);
+
+  // CLEAN
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot:any) => {
+          setPosts(snapshot.docs);
+          console.log(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
     return (
         <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
           <div className="text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700">
@@ -11,6 +47,15 @@ const Feed = () => {
             </div>
           </div>
           <Input />
+
+          <div className="pb-72">
+            {posts.map((post:any) => (
+              <Post key={post.id} id={post.id} post={post.data()}></Post>
+            ))
+
+            }
+          </div>
+
         </div>
     )
 }
